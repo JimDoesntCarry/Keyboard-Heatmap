ca01954cb73a3ec73f02ae1d8a4649dac9f9a02e
Powerkrieger <81979050+Powerkrieger@users.noreply.github.com>
2021-04-07 17:31:14 +0200

    brute force attempt
    unbuffered input into array, doesnt do the trick cause strg shift etc will not be read atm

--- Windows/main.cpp
+++ Windows/main.cpp
@@ -1,22 +1,52 @@
 #include <iostream>
 #include <windows.h>
+#define LENGTH 500
+
 using namespace std;
 
 int main()
-  {
-  HANDLE hstdin;
-  DWORD  mode;
+ {
+   HANDLE hstdin;
+   DWORD  mode;
+   int keys[LENGTH];
+   int i = 0;
 
   hstdin = GetStdHandle( STD_INPUT_HANDLE );
   GetConsoleMode( hstdin, &mode );
   SetConsoleMode( hstdin, ENABLE_ECHO_INPUT | ENABLE_PROCESSED_INPUT );  // see note below 
 
-  //cout << "Press any key..." << flush;
-  int ch = cin.get();
+  for(i= 0; i < LENGTH; i++){
+    keys[i]=0;
+  }
 
-  if (ch == 13) cout << "\nYou pressed ENTER\n";
-  else          cout << "\n\nYou did not press ENTER\n";
 
-  SetConsoleMode( hstdin, mode );
-  return 0;
+  cout << "Press any key..." << "\n" << flush;
+  while(true){
+      int ch = cin.get();
+      keys[ch]++;
+
+      if(ch == 13){
+        break;
+      }
+  }
+
+  for(i= 0; i < LENGTH; i++){
+    if(keys[i] > 0){
+      switch(i){
+        case 13: cout << "Key " << "Enter" << " pressed " << keys[i] << " times!" << "\n";break;
+        case 32: cout << "Key " << "Space" << " pressed " << keys[i] << " times!" << "\n";break;
+        case 97: cout << "Key " << "A" << " pressed " << keys[i] << " times!" << "\n";break;
+        case 115: cout << "Key " << "S" << " pressed " << keys[i] << " times!" << "\n";break;
+        case 100: cout << "Key " << "W" << " pressed " << keys[i] << " times!" << "\n";break;
+        case 119: cout << "Key " << "D" << " pressed " << keys[i] << " times!" << "\n";break;
+        
+        default: cout << "Key " << i << " pressed " << keys[i] << " times!" << "\n";break;
+      }
+       
+     
+    }
+  }
+  
+   SetConsoleMode( hstdin, mode );
+   return 0;
   }
